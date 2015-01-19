60b8a48c5b6de427d266e355c2404e8b454fb127
JP3BGY <heb52204@hcc5.bai.ne.jp>
2015-01-19 18:46:16 +0900

    Json読み込み可能にした

--- coffee/index.coffee
+++ coffee/index.coffee
@@ -1,2 +1,26 @@
-a -> a
+$.getJSON("json/indexnews.json",(data)->
+  allnewslines="<table><tbody>"
+  for newsline in data
+    allnewslines+="<tr><td class='toplistheader'>#{newsline.newsheader}</td><td class='toplistdate'>#{newsline.newsdate}</td></tr>\n"
+  allnewslines+="</tbody></table>"
+  $(allnewslines).appendTo("#newstable")
+  return
+).error(
+  (jqXHR,testStatus,errorThrown)->
+    $("#imglinks").html("access error #{jqXHR.responseText} #{errorThrown}")
+    return
+)
+
+$.getJSON("json/indexphotolinks.json",(data)->
+  allimglinks="<ul>"
+  for imgblock in data
+    allimglinks+="<li><a href=' #{imgblock.linkurl} '><img src='#{imgblock.imgsrc}' alt='#{imgblock.imgalt}'><div class='imgtext'>#{imgblock.imgtext}</div></a></li>"
+  allimglinks+="</ul>"
+  $(allimglinks).appendTo("#imglinks")
+  return
+).error(
+  (jqXHR,testStatus,errorThrown)->
+    $("#imglinks").html("access error #{jqXHR.responseText} #{errorThrown}")
+    return
+)
 
--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
-(function() {
-  a(function() {
-    return a;
-  });
+$.getJSON("json/indexnews.json", function(data) {
+  var allnewslines, newsline, _i, _len;
+  allnewslines = "<table><tbody>";
+  for (_i = 0, _len = data.length; _i < _len; _i++) {
+    newsline = data[_i];
+    allnewslines += "<tr><td class='toplistheader'>" + newsline.newsheader + "</td><td class='toplistdate'>" + newsline.newsdate + "</td></tr>\n";
+  }
+  allnewslines += "</tbody></table>";
+  $(allnewslines).appendTo("#newstable");
+}).error(function(jqXHR, testStatus, errorThrown) {
+  $("#imglinks").html("access error " + jqXHR.responseText + " " + errorThrown);
+});
 
-}).call(this);
+$.getJSON("json/indexphotolinks.json", function(data) {
+  var allimglinks, imgblock, _i, _len;
+  allimglinks = "<ul>";
+  for (_i = 0, _len = data.length; _i < _len; _i++) {
+    imgblock = data[_i];
+    allimglinks += "<li><a href=' " + imgblock.linkurl + " '><img src='" + imgblock.imgsrc + "' alt='" + imgblock.imgalt + "'><div class='imgtext'>" + imgblock.imgtext + "</div></a></li>";
+  }
+  allimglinks += "</ul>";
+  $(allimglinks).appendTo("#imglinks");
+}).error(function(jqXHR, testStatus, errorThrown) {
+  $("#imglinks").html("access error " + jqXHR.responseText + " " + errorThrown);
+});
